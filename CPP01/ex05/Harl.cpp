16515308729e5fb3dae1a753121cbc7e2d7d3205
magnitopic <magnitopic@gmail.com>
2023-11-09 20:07:47 +0100

    cpp01 ex05

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 21:11:08 by alaparic          #+#    #+#             */
-/*   Updated: 2023/11/08 21:20:16 by alaparic         ###   ########.fr       */
+/*   Updated: 2023/11/09 19:11:31 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,49 @@
 
 void Harl::complain(std::string level)
 {
-	
+	void (Harl::*complain_functions[4])(void) =
+	{
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
+	};
+	std::string valid_complaints[4] =
+	{
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"
+	};
+	bool flag = 1;
+	for (int i = 0; i < valid_complaints->length() - 1; i++)
+	{
+		if (level.compare(valid_complaints[i]) == 0)
+		{
+			(this->*complain_functions[i])();
+			flag = !flag;
+		}
+	}
+	if (flag)
+		std::cout << "\033[0;31m'" << level << "' is an invalid complaint level!\033[0m" << std::endl;
 }
 
 void Harl::debug()
 {
-	std::cout << "" << std::endl;
+	std::cout << "\033[0;35mI love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info()
 {
-	std::cout << "" << std::endl;
+	std::cout << "\033[0;34mI cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\033[0m" << std::endl;
 }
 
 void Harl::warning()
 {
-	std::cout << "" << std::endl;
+	std::cout << "\033[0;33mI think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\033[0m" << std::endl;
 }
 
 void Harl::error()
 {
-	std::cout << "" << std::endl;
+	std::cout << "\033[0;31mThis is unacceptable! I want to speak to the manager now.\033[0m" << std::endl;
 }
--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -6,12 +6,32 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 21:10:51 by alaparic          #+#    #+#             */
-/*   Updated: 2023/11/08 21:19:43 by alaparic         ###   ########.fr       */
+/*   Updated: 2023/11/09 19:12:02 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
 int main(int argc, char **argv)
 {
-	
+	Harl harl;
+
+	std::cout << "Let's do a test run" << std::endl;
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+
+	std::cout << std::endl
+			  << "--------------------" << std::endl
+			  << std::endl;
+
+	std::cout << "Some invalid values" << std::endl
+			  << std::endl;
+	std::cout << "Input: waring"<<std::endl;
+	harl.complain("waring");
+
+	std::cout << "Input: testing"<<std::endl;
+	harl.complain("testing");
 	return 0;
 }
