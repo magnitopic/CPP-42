e1e6ff5e91eb20c83fce8dc9f16706b44cca3e82
magnitopic <magnitopic@gmail.com>
2024-01-02 18:34:44 +0100

    starting cpp08

---
+++ CPP08/ex00/easyfind.hpp
@@ -0,0 +1,24 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/02 17:04:31 by alaparic          #+#    #+#             */
+/*   Updated: 2024/01/02 17:30:24 by alaparic         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#ifndef EASYFIND_HPP_
+#define EASYFIND_HPP_
+
+#include <iostream>
+
+template<typename T>
+void easyfind(T , int )
+{
+	for (int i = 0)
+}
+
+#endif
