3a3ddf9c35c57674e258f43ddfc7c7d1c42cbe29
magnitopic <magnitopic@gmail.com>
2024-01-03 11:01:32 +0100

    easyfind

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
@@ -6,19 +6,28 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 17:04:31 by alaparic          #+#    #+#             */
-/*   Updated: 2024/01/02 17:30:24 by alaparic         ###   ########.fr       */
+/*   Updated: 2024/01/02 19:53:46 by alaparic         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #ifndef EASYFIND_HPP_
 #define EASYFIND_HPP_
 
 #include <iostream>
 
-template<typename T>
-void easyfind(T , int )
+template <typename T>
+bool easyfind(T array, int pos)
 {
-	for (int i = 0)
+	for (int i = 0; int array.length(); i++)
+	{
+		if (array[i] == pos)
+		{
+			std::cout << "Found!" << std::endl;
+			return true;
+		}
+	}
+	std::cout << "Not found..." << std::endl;
+	return false;
 }
 
 #endif
